0b74c93022759ab280700da00c4f001cb75519ba
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 21:28:43 +0700

    menambahkan folder metode string

---
+++ #05 Metode_string/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Metode String</title>
+</head>
+
+<body>
+    <?php
+    $string1 = 'Ini adalah string sederhana';
+
+    $string2 = 'Ini adalah string yang bisa memiliki beberap baris';
+
+    $string3 = 'Dia berkata: "I\'ll be back"';
+
+    $string4 = 'Anda telah berhasil menghapus C:\\xampp\\htdoc';
+
+    $string5 = 'Kalimat ini tidak akan pindah ke: \n baris baru';
+
+    $string6 = 'Variabel juga tidak otomatis ditampilkan $string1 dan $string3';
+
+    echo $string1;
+    echo "<br>";
+    echo $string2;
+    echo "<br>";
+    echo $string3;
+    echo "<br>";
+    echo $string4;
+    echo "<br>";
+    echo $string5;
+    echo "<br>";
+    echo $string6;
+    ?>
+</body>
+
+</html>
