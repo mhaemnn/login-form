b69581794c1e316ee5680b8faaec67bacc05e1d9
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 21:23:26 +0700

    menambahkan folder operator aritmatika

---
+++ #04 Operator Aritmatika/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Opertor Aritmatika</title>
+</head>
+
+<body>
+    <?php
+    // operator aritmaika dalam php
+
+    $a = 100;
+    $b = 10;
+    // penjumlahan
+    $c = $a + $b;
+    echo "$a + $b = $c";
+    echo "<hr>";
+
+    // pengurangan
+    $c =  $a - $b;
+    echo "$a - $b";
+    echo "<hr>";
+
+    // perkalian
+    $c = $a * $b;
+    echo "$a * $b";
+    echo "<hr>";
+
+    // pembagian
+    $c = $a / $b;
+    echo "$a / $b";
+    echo "<hr>";
+
+    // modulus
+    $c = $a % $b;
+    echo "$a % $b";
+    echo "<hr>";
+
+    // pemangkatan
+    $c = $a ** $b;
+    echo "$a ** $b";
+    echo "<hr>";
+    ?>
+</body>
+
+</html>
