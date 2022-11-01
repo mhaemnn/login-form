20af979b0971df06932452a28626172a53c492f6
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 20:19:30 +0700

    upload folder tipe data

---
+++ #03 Tipe Data/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tipe Data</title>
+</head>
+
+<body>
+    <?php
+    // tipe data:
+
+    // tipe data char
+    $jenisKelamin = "L";
+    // tipe data string 
+    $nama_lengkap = "nanangsurotong";
+    // tipe data int
+    $umur  = 10;
+    // tipe data float
+    $berat = 50.4;
+    // tipe data boolean
+    $menikah = false;
+
+    echo "Nama: $nama_lengkap <br>";
+    echo "Jenis Kelamin: $jenisKelamin <br>";
+    echo "umur: $umur tahu <br>";
+    echo "berat: $berat kg <br>";
+    echo "menikah: $menikah";
+
+
+    ?>
+</body>
+
+</html>
