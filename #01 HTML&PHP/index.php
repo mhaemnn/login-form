37b328dd749afae2d63ab36ade711f617aecb9db
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 16:34:38 +0700

    menambahkan folder HTML & PHP

--- #01 HTML&PHP/index.php
+++ #01 HTML&PHP/index.php
@@ -7,7 +7,12 @@
 
 <body>
     <?php
+    echo "<h2>Belajar PHP itu mudah!</h2>";
     echo "Belajar itu mudah yang susah itu konsiten-nya";
+    echo ("Hello world!<br>");
+    echo "Aku sedang belajar PHP!<br>";
+    echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
     ?>
 </body>
 
