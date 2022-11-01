4a1fd4252f6b0eb0bea190d936d626a6e65b182c
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 20:45:26 +0700

    upload file index dalam foler tipe data

--- #03 Tipe Data/index.php
+++ #03 Tipe Data/index.php
@@ -25,6 +25,44 @@
     echo "umur: $umur tahu <br>";
     echo "berat: $berat kg <br>";
     echo "menikah: $menikah";
+    echo "==================================";
+
+    // tipe data char dan string
+    $alamat = "JL Javascript No.23, Jakrta ";
+
+    // tipe data int
+    $point = -21;
+    $nilai = 23;
+
+    // tipe data float 
+    $panjang = 234.12;
+    $kapasistas = 12330.90;
+
+    $jarak = 1.2E-5;
+    // tipe data float dapat ditulis dalam notasi seperti ini.
+    // E-5 adalah eksponen dari 10 contoh ini sama saja dengan 1.2 x 10-5. kalau di jabarkan akan menjadi 
+    // 0.000012
+    // agar format float tidak tercetak dalam nota E bisa menggunakan fungsi sprintf()
+
+    echo sprintf('%f', $a);
+    // batasi angka dibelakang koma
+    echo sprintf('%.3f', $a);
+
+    // tipe data array dalam php
+    $minuman = array("kopi", "teh", "susu");
+    $makanan = ["nasgor", "seblak", "bubur"];
+
+    // tipe data NULL
+    $nama =  NULL;
+
+    // konversi tipe data php versi bahasa C
+    $angka = "32";
+    $angka = (int) $angka; // merubah nilai angka menjadi int
+    $angka = (float) $angka; // merubah nilai angka menjadi float
+    $angka = (string) $angka; // merubah nilai angka menjadi string
+
+
+
 
 
     ?>
