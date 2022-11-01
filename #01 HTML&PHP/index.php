626500fa6bb2250875e2e721311290f4296b38e9
mhaemnn <ismuhaimin1@gmail.com>
2022-11-01 16:19:30 +0700

    menambahkan file index.php

---
+++ #01 HTML&PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HTML & PHP</title>
+</head>
+
+<body>
+    <?php
+    echo "Belajar itu mudah yang susah itu konsiten-nya";
+    ?>
+</body>
+
+</html>
