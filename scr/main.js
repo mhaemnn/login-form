b178cf68dd00556defd13c8c1d960fd42764b336
mhaemnn <ismuhaimin1@gmail.com>
2023-03-23 13:55:57 +0700

    delete code

--- scr/main.js
+++ scr/main.js
@@ -1,2 +1,18 @@
 const loginText = document.querySelector(".title-login .login");
 const loginForm = document.querySelector("form .login");
+const loginBtn = document.querySelector("label .login");
+const signupBtn = document.querySelector("label .login");
+const signupLink = document.querySelector("form .signup-link a ");
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+  signupBtn.onclick();
+  return false;
+};
